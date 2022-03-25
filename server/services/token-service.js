fea2ec1c5a6993ef0d2acf570bdb53cdc6357987
romanivn415 <ronamivn415@gmail.com>
2022-03-26 00:03:00 +0300

    25.03.22

--- server/services/token-service.js
+++ server/services/token-service.js
@@ -1,10 +1,23 @@
 const jwt = require('jsonwebtoken')
+const tokenModel = require('../models/token-modle')
 
 class TokenServise{
     generateToken(payload){
         const accessToken = jwt.sign(payload, process.env.JWT_ACCESS, {expiresIn: '30m'});
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH, {expiresIn: '30d'});
         return {accessToken, refreshToken}
+    }
+
+    async saveToken(userId, refreshToken){
+        const tokenData = await tokenModel.findOne({user: userId});
+
+        if (tokenData){
+            tokenData.refreshToken == refreshToken
+            return tokenData.save()
+        }
+
+        const token = tokenModel.create({user: userId, refreshToken})
+        return token
     }
 }
 
--- server/services/user-service.js
+++ server/services/user-service.js
@@ -1,19 +1,27 @@
-const UserModule = require('../models/user-model');
+const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
+const tokenServise = require('../services/token-service');
+const UserDto = require('../dtos/user-dto')
 
 class UserService{
     async registration(email, password){
-        const candidate = await UserModule.findOne({email});
+        const candidate = await UserModel.findOne({email});
 
         if (candidate ) throw new Error(`User with email ${email} already exists`)
 
         const hashPassword = await bcrypt.hash(password, 3);
         const activationLink = uuid.v4();
-        const user = await UserModule.create({email, password: hashPassword, activationLink});
+        const user = await UserModel.create({email, password: hashPassword, activationLink});
+        await mailService.sendActivationMail(email, process.env.API_URL + '/api/activate/' + activationLink)
 
-        await mailService.sendActivationMail(email, activationLink)
+        const payload = new UserDto(user)
+        const token = tokenServise.generateToken({...payload})
+
+        await tokenServise.saveToken(payload.id, token.refreshToken)
+
+        return {...token, user: payload}
     }
 };
 
